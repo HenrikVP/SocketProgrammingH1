6e35ade3ef5cd456e6b17293062dc31ab8444ce2
hpo@tec.dk <hpo@tec.dk>
2023-06-20 08:54:25 +0200

    Add project files.

---
+++ SocketProgrammingServer/SocketServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace SocketProgrammingServer
+{
+    internal class SocketServer
+    {
+        internal void StartServer()
+        {
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(
+                Dns.GetHostName(), 
+                System.Net.Sockets.AddressFamily.InterNetwork
+                );
+            IPAddress iPAddress = iPHostEntry.AddressList[0];
+            //Or convert a string to type of IpAddress
+            //IPAddress iPAddress2 = IPAddress.Parse("192.168.1.2");
+            IPEndPoint iPEndPoint = new(iPAddress, 22222);
+        }
+
+    }
+}
